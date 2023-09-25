d48a99afc1ff62bf2b0598f514d9ff6cd8e3e0c1
Mirovinkin <vaheabroyan3@gmail.com>
2023-09-25 19:15:57 +0500

    Добавьте файлы проекта.

---
+++ Lesson25092023/Lesson25092023.cpp
@@ -0,0 +1,51 @@
+﻿// Lesson25092023.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <windows.h>
+#include <locale.h>
+#include <string.h>
+#include <typeinfo>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+#include <cstdlib>
+#include <ctype.h>
+
+using namespace std;
+
+class Progress
+{
+private:
+    double x;
+    double y;
+
+public:
+    void Print() {
+        cout << "Координат x:" << "\nКоординат y:" << y;
+    }
+
+    void Set(const double x, double y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    double Sum() {
+        return x + y;
+    }
+    double Max() {
+        return std::max(x, y);
+    }
+    double Max() {
+        return std::min(x, y);
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+
+}
